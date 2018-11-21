691d59dc63932b64d6057565b494126e7386a15a
Kobe Yu <kobe0308@gmail.com>
2018-11-21 16:42:28 +0800

    add trining script

--- research/train_farmbot.sh
+++ research/train_farmbot.sh
@@ -1,5 +1,5 @@
 python object_detection/model_main.py \
-    --pipeline_config_path=./object_detection/farmbot/model/ssd_mobilenet_v1_pets.config \
+    --pipeline_config_path=./object_detection/farmbot/model/ssd_mobilenet_v1_tomato.config \
     --model_dir=./object_detection/farmbot/model/ \
     --num_train_steps=200000 \
     --num_eval_steps=8000 \
