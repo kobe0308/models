39fe84b8cf1279e4ad6388d4f227bc70fa44b61a
Kobe Yu <kobe0308@gmail.com>
2018-11-21 16:41:16 +0800

    adding setup model script

---
+++ research/object_detection/farmbot/model/setup_model.sh
@@ -0,0 +1,7 @@
+wget wget http://download.tensorflow.org/models/object_detection/ssd_mobilenet_v1_coco_11_06_2017.tar.gz
+tar zxvf ssd_mobilenet_v1_coco_11_06_2017.tar.gz
+mv ssd_mobilenet_v1_coco_11_06_2017/* .
+rm -rf ssd_mobilenet_v1_coco_11_06_2017
+rm ssd_mobilenet_v1_coco_11_06_2017.tar.gz
+
+
